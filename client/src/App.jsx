3e00d4f3f36c1cbbe71147cd13b4fcacc86d5048
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-10-16 15:07:27 +0200

    leaderboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import QuizPage from "./Pages/QuizPage";
 import FinalPage from "./Pages/FinalPage";
+import Leaderboard from "./Pages/Leaderboard"
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
           <Route path="/Home" element={<HomePage />} />
           <Route path="/Quiz" element={<QuizPage />} />
           <Route path="/Final" element={<FinalPage />} />
+          <Route path="/Leaderboard" element={<Leaderboard />} />
         </Routes>
       </Router>
     </>
