29be97ce28596747d2c0d3fa59582f0a03716d34
DarrellRoberts <mrdarrellroberts@gmail.com>
2023-10-16 15:40:00 +0200

    added loader

--- client/src/Pages/Leaderboard.jsx
+++ client/src/Pages/Leaderboard.jsx
@@ -1,6 +1,9 @@
 import {useState, useEffect} from "react"
+import {NavLink} from "react-router-dom"
+import "../App.css"
 
 export default function Leaderboard() {
+    const [isLoading, setIsLoading] = useState(true);
     const [userData, setUserData] = useState([])
 
     const getData = async () => {
@@ -9,20 +12,27 @@ export default function Leaderboard() {
         setUserData(data.users);
     }
 
-
     useEffect(() => {
-        getData()
-    }, [])
+        const timer = setTimeout(() => {
+          setIsLoading(false);
+          getData();
+        }, 5000);
+        return () => clearTimeout(timer);
+      }, []);
 
     const sortedLeaderboard = userData.sort((a,b) => b.points-a.points)
     console.log(userData);
     console.log(userData.map((user) => user.username));
     return (
         <>
+    <NavLink to="/">Home</NavLink>
         <h1 style={{textAlign: "center"}}>Leaderboard</h1>
         <div>
+        {isLoading ? (
+        <h2 className="questionLoader" style={{ fontSize: "20rem", textAlign: "center" }}>?</h2>
+        ) : null}
         {userData.map((user, index) => (
-            <div key={index} style={{display: "flex", wrap: "wrap", justifyContent: "space-evenly", backgroundColor: "grey", alignItems: "center"}}>
+            <div key={index} style={{display: "flex", wrap: "wrap", justifyContent: "space-evenly", alignItems: "flex-start", backgroundColor: "grey", alignItems: "center"}}>
                 <h2>{`#${index + 1}`} {user.username}</h2>
                 <h2>Score: {user.score}</h2>
                 <h2>Total Games: {user.games_total}</h2>
